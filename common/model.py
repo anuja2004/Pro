58a7644fe677c8daa8d945e51b9cf3647accf6cc
Anuja Suntnur <126876878+anuja2004@users.noreply.github.com>
2026-04-03 23:10:43 +0530

    Delete common directory

--- common/model.py
+++
@@ -1,8 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-def get_model():
-    return LogisticRegression(
-        max_iter=300,
-        class_weight="balanced",
-        solver="lbfgs",
-    )
